4017624b5937721a774f7768f62f9bacf206eb1d
Egor Zapatylak <zapatylak96a@gmail.com>
2022-09-16 08:41:29 +0300

    Simple refactoring code

--- src/App.tsx
+++ src/App.tsx
@@ -26,10 +26,10 @@ function App() {
     let tasksForTodolist = tasks;
 
     if (filter === "active") {
-        tasksForTodolist = tasks.filter(tasks => tasks.isDone === false)
+        tasksForTodolist = tasks.filter(tasks => tasks.isDone)
     }
     if (filter === "completed") {
-        tasksForTodolist = tasks.filter(tasks => tasks.isDone === true)
+        tasksForTodolist = tasks.filter(tasks => !tasks.isDone)
     }
 
     function changeFilter(value: filterValueType ) {
